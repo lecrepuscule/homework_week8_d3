b538730332785876eff4bd1501c185bd6aaa47e2
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-30 23:22:26 +0100

    added routes config

--- config/routes.js
+++ config/routes.js
@@ -2,8 +2,29 @@ var express = require("express");
 var usersRouter = express.Router();
 var chatsRouter = express.Router();
 
+var passport = require("passport");
+var bodyParser = require("body-parser");
+
 var usersController = require("../controllers/users");
 var chatsController = require("../controllers/channels");
 
 
-chatsRouter.route("/channels")
+function authenticatedUser(req, res, next){
+  (req.isAuthenticated()) ? next() : res.redirect("/");
+}
+
+usersRouter.route('/')
+  .get(staticsController.home);
+
+usersRouter.route('/signup')
+  .get(usersController.getSignup)
+  .post(usersController.postSignup)
+
+usersRouter.route('/login')
+  .get(usersController.getLogin)
+  .post(usersController.postLogin)
+
+usersRouter.route("/logout")
+  .get(usersController.getLogout)
+
+module.exports = usersRouter;
